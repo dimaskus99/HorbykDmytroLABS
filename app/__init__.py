228581037f4479304cfc7f7f390fd83c2ab2d432
dimaskus99 <horbyk.dmytro2017@gmail.com>
2024-12-15 16:23:09 +0200

    update init

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,25 @@
 from flask import Flask
-app = Flask(__name__)
-from app import views
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from .config import Config
+from .utils import register_error_handlers
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    from app.views import api_bp
+    app.register_blueprint(api_bp)
+
+    register_error_handlers(app)
+
+    return app
+
+
 
